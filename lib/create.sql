0ebdcc4a8dd4070b54d337bab9654bad42880fb0
Samson Mubeya <rasspartah@gmail.com>
2024-07-05 00:52:01 +0300

    wrote sql code to manipulate and querry seed.sql

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE IF NOT EXISTS bears (
+    id INTEGER PRIMARY KEY,
+    name INTEGER,
+    age INTEGER,
+    sex BOOLEAN,
+    color TEXT,
+    temperament INTEGER,
+    alive BOOLEAN
+
+);
